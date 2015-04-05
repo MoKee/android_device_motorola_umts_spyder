cd370be448c5a717600dc6508468f63c9b5ce15c
martincz <martincz@mokeedev.com>
2015-04-05 16:29:44 +0800

    mokee bringup
    Change-Id: I89c2da6201ddb2d6fe9a6f412b333f86096a626d

--- mokee.mk
+++ mokee.mk
@@ -1,13 +1,13 @@
 # Inherit device configuration for VZW Droid RAZR.
 $(call inherit-product, device/motorola/umts_spyder/full_umts_spyder.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common MK stuff.
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
 
 #
 # Setup device specific product configuration.
 #
-PRODUCT_NAME := cm_umts_spyder
+PRODUCT_NAME := mk_umts_spyder
 PRODUCT_BRAND := RTGB
 PRODUCT_DEVICE := umts_spyder
 PRODUCT_MODEL := XT910
@@ -25,6 +25,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
    BUILD_FINGERPRINT=${PRODUCT_BRAND}/${PRODUCT_DEVICE}_${PRODUCT_SFX}/${PRODUCT_DEVICE_PREFIX}_${PRODUCT_DEVICE}:${PLATFORM_VERSION}/${BUILD_ID}/${DATE}:user/release-keys \
    PRIVATE_BUILD_DESC="${PRODUCT_DEVICE_PREFIX}_${PRODUCT_DEVICE}-user ${PLATFORM_VERSION} ${BUILD_ID} ${DATE} release-keys" \
    PRODUCT_BRAND=${PRODUCT_BRAND} \
-   BUILD_UTC_DATE= \
-   PRODUCT_DEFAULT_LANGUAGE=en \
-   PRODUCT_DEFAULT_REGION=US \
+   BUILD_UTC_DATE= 
